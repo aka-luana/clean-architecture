68c4a04c03bf3ae56503ea429d10bb425f363c49
Luana Matos Brito Sales <luana.sales@ambevtech.com.br>
2023-05-18 21:37:49 -0300

    Criado o service

---
+++ CleanArchMvc.Application/Services/ProductService.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+
+namespace CleanArchMvc.Application.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _repository;
+        private readonly IMapper _mapper;
+
+        public ProductService(IProductRepository productRepository, IMapper mapper)
+        {
+            _repository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+            _mapper = mapper;
+        }
+
+        public async Task<ProductDTO> GetById(int? id)
+        {
+            var productEntity = await _repository.GetByIdAsync(id);
+
+            return _mapper.Map<ProductDTO>(productEntity);
+        }
+
+        public async Task<ProductDTO> GetProductCategory(int? id)
+        {
+            var productEntity = await _repository.GetProductCategoryAsync(id);
+
+            return _mapper.Map<ProductDTO>(productEntity);
+        }
+
+        public async Task<IEnumerable<ProductDTO>> GetProducts()
+        {
+            var productsEntity = await _repository.GetProductAsync();
+
+            return _mapper.Map<IEnumerable<ProductDTO>>(productsEntity);
+        }
+
+        public async Task Add(ProductDTO productDTO)
+        {
+            var productEntity = _mapper.Map<Product>(productDTO);
+
+            await _repository.CreateAsync(productEntity);
+        }
+
+        public async Task Remove(int? id)
+        {
+            var productEntity = _repository.GetByIdAsync(id).Result;
+
+            await _repository.RemoveAsync(productEntity);
+        }
+
+        public async Task Update(ProductDTO productDTO)
+        {
+            var productEntity = _mapper.Map<Product>(productDTO);
+
+            await _repository.UpdateAsync(productEntity);
+        }
+    }
+}
