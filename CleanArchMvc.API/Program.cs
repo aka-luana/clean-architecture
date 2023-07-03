228b3633b2888edc059d14bef45a8f8b11e3b1b1
Luana Matos Brito Sales <luana.sales@ambevtech.com.br>
2023-07-03 19:47:12 -0300

    Ativando autenticação e validando token

--- CleanArchMvc.API/Program.cs
+++ CleanArchMvc.API/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddInfraestructureAPI(builder.Configuration);
+builder.Services.AddInfraestructureJWT(builder.Configuration);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
---
+++ CleanArchMvc.Infra.IoC/DependencyInjectionJWT.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace CleanArchMvc.Infra.IoC
+{
+    public static class DependencyInjectionJWT
+    {
+        public static IServiceCollection AddInfraestructureJWT(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Informar o tipo de autenticação JWT-Bearer
+            // Definir o modelo de desafio de autenticação
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            // Hebilita a autenticação JWT usadno o esquema e desafio definidos
+            // Validar o token
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = configuration["Jwt:Issuer"],
+                    ValidAudience = configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:SecretKey"])),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
+            return services;
+        }
+    }
+}
